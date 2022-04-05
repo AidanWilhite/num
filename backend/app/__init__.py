d9a139d8b6e67704046cfb46c9f7652b70342b9d
toco4time <80077176+toco4time@users.noreply.github.com>
2022-04-04 21:24:01 -0500

    everything is broken :(

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,8 +10,8 @@ def create_app():
     app = Flask(__name__)
     #app.config['SECRET_KEY'] = 'gbyaeduhbifewh9upgh8u9ier9phgqh9iu8erghi809'
     app.config['SECRET_KEY'] = 'the random string'
-    app.template_folder = os.path.abspath('frontend')
-    # print(app.template_folder)
+    app.template_folder = 'backend/frontend/'
+    print(app.template_folder)
     app.register_blueprint(Guess)
     app.register_blueprint(Set)
 
